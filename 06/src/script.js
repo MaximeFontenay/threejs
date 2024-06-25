266aa5cf666394a2f42306c16403fd556e55e522
MaximeFontenay <maximefontenay1@gmail.com>
2024-06-25 12:03:24 +0200

    08

--- 06/src/script.js
+++ 06/src/script.js
@@ -1,84 +1,74 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-const cursor = {
-  x: 0,
-  y: 0,
-};
-
-window.addEventListener("mousemove", (event) => {
-  cursor.x = event.clientX / sizes.width - 0.5;
-  cursor.y = -(event.clientY / sizes.height - 0.5);
-});
-
-/**
- * Base
- */
-// Canvas
 const canvas = document.querySelector("canvas.webgl");
-
-// Sizes
-const sizes = {
-  width: 1180,
-  height: 600,
-};
-
-// Scene
 const scene = new THREE.Scene();
 
-// Object
-const mesh = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-  new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: false })
-);
+const size = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+const geometry = new THREE.BufferGeometry();
+
+const count = 5;
+const positionsArray = new Float32Array(count * 3 * 3);
+for (let i = 0; i < count * 3 * 3; i++) {
+  positionsArray[i] = (Math.random() - 0.5) * 4;
+}
+
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
+geometry.setAttribute("position", positionsAttribute);
+
+const material = new THREE.MeshBasicMaterial({
+  color: 0x83f265,
+  wireframe: true,
+});
+
+const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-// Camera
 const camera = new THREE.PerspectiveCamera(
-  50,
-  sizes.width / sizes.height,
+  75,
+  size.width / size.height,
   0.1,
-  100
+  1000
 );
-
-// camera.position.x = 2;
-// camera.position.y = 2;
-camera.position.z = 4;
+camera.position.z = 2;
 camera.lookAt(mesh.position);
 scene.add(camera);
 
-// Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
-// Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
+  antialias: true,
 });
-renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-// Animate
 const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
-
-  // Update objects
-  //mesh.rotation.y = elapsedTime;
-
-  //   camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
-  //   camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
-  //   camera.position.y = cursor.y * 10;
-  //   camera.lookAt(mesh.position);
-
-  // Update controls
   controls.update();
-
-  // Render
   renderer.render(scene, camera);
-
-  // Call tick again on the next frame
   window.requestAnimationFrame(tick);
 };
 
 tick();
+
+const renderCanvas = () => {
+  size.width = window.innerWidth;
+  size.height = window.innerHeight;
+
+  camera.aspect = size.width / size.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(size.width, size.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+};
+
+renderCanvas();
+
+window.addEventListener("resize", renderCanvas);
